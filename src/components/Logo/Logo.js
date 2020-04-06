712213b7469a6e2a419bdbfae6e728d22ccd4ccd
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2020-04-07 00:00:57 +0530

    Added Logo of our App in Toolbar
    - Added Logo in Assets Folder.
    - Created Component Logo.

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import styles from "./Logo.module.css";
+import burgerLogo from "../../assets/images/burger-builder-logo.png";
+
+const logo = () => {
+  return (
+    <div className={styles.Logo}>
+      <img src={burgerLogo} alt="Burger Builder" />
+    </div>
+  );
+};
+
+export default logo;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,11 +1,14 @@
 import React from "react";
+
+import Logo from "../../Logo/Logo";
+
 import styles from "./Toolbar.module.css";
 
 const toolbar = (props) => {
   return (
     <header className={styles.Toolbar}>
       <div>Menu</div>
-      <div>LOGO</div>
+      <Logo />
       <nav>
         <ul>...</ul>
       </nav>
