8230bb36654a149a37a131ddb0b077243f7b07ee
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2020-04-06 15:25:19 +0530

    Added functionality for More button in Build Controls
    - Added State of Total Cost in App
    - Added individual costs of Ingredients
    - Added handler for quantity addition : addIngredientHandler()

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -13,7 +13,13 @@ const buildControls = (props) => {
   return (
     <div className={styles.BuildControls}>
       {controls.map((control) => (
-        <BuildControl key={control.label} label={control.label} />
+        <BuildControl
+          key={control.label}
+          label={control.label}
+          added={() => {
+            props.ingredientAdded(control.type);
+          }}
+        />
       ))}
     </div>
   );
