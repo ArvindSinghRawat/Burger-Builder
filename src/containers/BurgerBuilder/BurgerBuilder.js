50a4a0074dbc105ed377ed5f44b2509922fce71e
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2020-04-06 15:14:31 +0530

    Added Build Controls in App (Non Working Components)
    - Added Build Control in Build Controls
    - Added Build Control in App

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Aux from '../../hoc/Auxiliary'
 import Burger from "../../components/Burger/Burger"
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 
 export class BurgerBuilder extends Component {
 
@@ -17,7 +18,7 @@ export class BurgerBuilder extends Component {
         return (
             <Aux>
                 <div><Burger ingredients={this.state.ingredients} /></div>
-                <div>Builder Controls</div>
+                <div><BuildControls /></div>
             </Aux>
         )
     }
