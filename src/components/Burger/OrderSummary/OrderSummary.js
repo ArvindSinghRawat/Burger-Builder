806b6bc875f4cf333b05a434b8168672e22801be
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2020-04-06 23:38:16 +0530

    Added Total Price in Order Summary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -16,6 +16,10 @@ const orderSummary = (props) => {
       <h3>Your Order</h3>
       <p>A delicious burger with the following ingredients : </p>
       <ul>{ingredientSummary}</ul>
+      <p>
+        <strong>Total Price : $ {props.price.toFixed(2)}</strong>
+      </p>
+      <br />
       <p>Continue to Checkout</p>
       <Button btnType="Success" clicked={props.purchseContinued}>
         CONTINUE
