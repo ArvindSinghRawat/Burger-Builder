d8c142c8a91ed968913ea1ad61570c68809977b9
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2020-04-06 14:55:05 +0530

    Added empty burger message in Burger Component.
    - Reduced transformedIngredients from Array of Arrays to singleton
    Array.
    - Added length check, to check whether any ingredient exist or not.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,22 @@ import styles from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients).map(
+  let transformedIngredients = Object.keys(props.ingredients).map(
     (ingredientKey) => {
       return [...Array(props.ingredients[ingredientKey])].map((_, index) => (
         <BurgerIngredient key={ingredientKey + index} type={ingredientKey} />
       ));
     }
-  );
+  ).reduce((arr, el) => {
+      return arr.concat(el); 
+  }, []);
+
+  if (transformedIngredients.length === 0)
+  {
+      transformedIngredients = <p>
+          Please start Adding Ingredients
+      </p>
+  }
 
   console.log(transformedIngredients);
 
