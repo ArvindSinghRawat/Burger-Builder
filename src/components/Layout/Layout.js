2eed27328665bef1cc3fdc8b05cdfdb640afeb2a
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2020-04-07 00:29:14 +0530

    Added Sidedrawer component in App
    - Added Sidedrawer component in Layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,16 @@
 import React from "react";
 
 import Aux from "../../hoc/Auxiliary";
-import Toolbar from "../../components/Navigation/Toolbar/Toolbar"
-import styles from './Layout.module.css'
+import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
+import Sidedrawer from "../Navigation/SideDrawer/SideDrawer";
+
+import styles from "./Layout.module.css";
 
 const layout = (props) => {
   return (
     <Aux>
       <Toolbar />
+      <Sidedrawer />
       <div>Sidedrawer, Backdrop</div>
       <main className={styles.Content}>{props.children}</main>
     </Aux>
