2433300bd587e0a59f315b5e67ac9a93005cb689
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2020-04-06 23:23:37 +0530

    Added Button as UI Component
    - Adde Button as UI Component
    - Added Dummy Continue and Cancel buttons in Order Summary.

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Aux from "../../../hoc/Auxiliary";
-
+import Button from "../../UI/Button/Button";
 const orderSummary = (props) => {
   const ingredientSummary = Object.keys(props.ingredients).map(
     (ingredientKey) => (
@@ -16,6 +16,8 @@ const orderSummary = (props) => {
       <p>A delicious burger with the following ingredients : </p>
       <ul>{ingredientSummary}</ul>
       <p>Continue to Checkout</p>
+      <Button>CONTINUE</Button>
+      <Button>CANCEL</Button>
     </Aux>
   );
 };
