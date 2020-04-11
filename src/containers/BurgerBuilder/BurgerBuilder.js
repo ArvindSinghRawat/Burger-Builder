29ca0b701547bbc78f9653f66b8f2711c8b07cef
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2020-04-11 13:27:10 +0530

    Added functionality to pass the ingredients to Checkout Page
    - Replaced state from individual ingredients to ingredients dictionary.
    - Sent ingredients from Burger builder via Query Params.
    - Received ingredients in Checkout page via Query params and feeded to
    component state.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -129,7 +129,19 @@ class BurgerBuilder extends Component {
     //       purchasing: false,
     //     });
     //   });
-    this.props.history.push("/checkout");
+    const queryParams = [];
+    for (let ingredient in this.state.ingredients) {
+      queryParams.push(
+        encodeURIComponent(ingredient) +
+          "=" +
+          encodeURIComponent(this.state.ingredients[ingredient])
+      );
+    }
+    const queryString = queryParams.join("&");
+    this.props.history.push({
+      pathname: "/checkout",
+      search: "?" + queryString,
+    });
   };
 
   render() {
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -4,11 +4,22 @@ import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSumm
 
 export class Checkout extends Component {
   state = {
-    salad: 1,
-    meat: 1,
-    bacon: 1,
-    cheese: 1,
+    ingredients: {
+      salad: 1,
+      meat: 1,
+      bacon: 1,
+      cheese: 1,
+    },
   };
+
+  componentDidMount() {
+    const query = new URLSearchParams(this.props.location.search);
+    const ingredients = {};
+    for (let param of query.entries()) {
+      ingredients[param[0]] = +param[1];
+    }
+    this.setState({ ingredients: ingredients });
+  }
 
   checkoutCancelledHandler = () => {
     this.props.history.goBack();
@@ -22,7 +33,7 @@ export class Checkout extends Component {
     return (
       <div>
         <CheckoutSummary
-          ingredients={this.state}
+          ingredients={this.state.ingredients}
           checkoutCancelled={this.checkoutCancelledHandler}
           checkoutContinued={this.checkoutContinuedHandler}
         />
