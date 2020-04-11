a1ef159bb5c4d728bb7a261d07eae798ae148cb8
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2020-04-11 13:04:05 +0530

    Connected Checkout to Burger Builder
    - When on Checkout Modal, continue button is clicked, the page is
    redirected to the Checkout page
    - Removed Content from Burger builder that is responsible for saving
    orders to Database.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -95,40 +95,41 @@ class BurgerBuilder extends Component {
   };
 
   continuePurchaseHandler = () => {
-    this.setState({
-      loading: true,
-    });
+    // this.setState({
+    //   loading: true,
+    // });
 
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.price,
-      customer: {
-        name: "Arvind",
-        address: {
-          street: "Teststreet 1",
-          zipcode: "41532",
-          country: "India",
-        },
-        email: "test@test.com",
-      },
-      deliveryMethod: "COD",
-    };
-    axios
-      .post("/orders.json", order)
-      .then((response) => {
-        console.log(response);
-        this.setState({
-          loading: false,
-          purchasing: false,
-        });
-      })
-      .catch((error) => {
-        console.log(error);
-        this.setState({
-          loading: false,
-          purchasing: false,
-        });
-      });
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.price,
+    //   customer: {
+    //     name: "Arvind",
+    //     address: {
+    //       street: "Teststreet 1",
+    //       zipcode: "41532",
+    //       country: "India",
+    //     },
+    //     email: "test@test.com",
+    //   },
+    //   deliveryMethod: "COD",
+    // };
+    // axios
+    //   .post("/orders.json", order)
+    //   .then((response) => {
+    //     console.log(response);
+    //     this.setState({
+    //       loading: false,
+    //       purchasing: false,
+    //     });
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //     this.setState({
+    //       loading: false,
+    //       purchasing: false,
+    //     });
+    //   });
+    this.props.history.push("/checkout");
   };
 
   render() {
