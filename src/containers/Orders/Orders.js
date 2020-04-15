a5740872b1545929c81322e33b21ff25a6bdedd2
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2020-04-15 13:53:54 +0530

    Fetched orders from Datbase in Orders Components.

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,13 +1,41 @@
 import React, { Component } from "react";
+import axios from "../../axios-orders";
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 
 import Order from "../../components/Order/Order";
 
 export class Orders extends Component {
+  state = {
+    orders: [],
+    loading: true,
+  };
+  componentDidMount() {
+    axios
+      .get("/order")
+      .then((response) => {
+        const fetchedOrders = [];
+        for (let key in response.data) {
+          fetchedOrders.push({ ...response.data[key], id: key });
+        }
+        this.setState({
+          loading: false,
+          orders: fetchedOrders,
+        });
+      })
+      .catch((error) => {
+        this.setState({
+          loading: false,
+        });
+      });
+  }
   render() {
-    return <div>
+    console.log(this.state);
+    return (
+      <div>
         <Order />
-    </div>;
+      </div>
+    );
   }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
