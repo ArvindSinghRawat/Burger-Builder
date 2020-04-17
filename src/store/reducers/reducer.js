520b5a6749b3676bcb7b85de1521710455c39bf6
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2020-04-17 12:42:48 +0530

    Added logic for acitons of reducers.
    - Made deep-copy of states to create changes.

--- src/store/reducers/reducer.js
+++ src/store/reducers/reducer.js
@@ -1,21 +1,39 @@
 import * as actionTypes from "../actions";
 
 const initalState = {
-  ingredients: null,
+  ingredients: {
+    salad: 0,
+    bacon: 0,
+    cheese: 0,
+    meat: 0,
+  },
   totalPrice: 4,
 };
 
 const reducer = (state = initalState, action) => {
-  const newState = { ...state };
-  switch (action.type) {
-    case actionTypes.ADD_INGREDIENT:
-      break;
-    case actionTypes.REMOVE_INGREDIENT:
-      break;
-    default:
-      break;
+  if (action) {
+    switch (action.type) {
+      case actionTypes.ADD_INGREDIENT:
+          return {
+              ...state,
+              ingredients: {
+                  ...state.ingredients,
+                  [action.ingredientName]: state.ingredients[action.ingredientName] + 1,
+              }
+          }
+      case actionTypes.REMOVE_INGREDIENT:
+        return {
+            ...state,
+            ingredients: {
+                ...state.ingredients,
+                [action.ingredientName]: state.ingredients[action.ingredientName] - 1,
+            }
+        }
+      default:
+        break;
+    }
   }
-  return newState;
+  return state;
 };
 
 export default reducer;
