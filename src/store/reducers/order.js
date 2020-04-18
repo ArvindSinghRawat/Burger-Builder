a1cc52512f8b88af21dda33346cd5d46244631de
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2020-04-18 13:21:08 +0530

    Added reducer for handling  Purchase of burger.
    - Added Order's Reducer's state orders and loading.

---
+++ src/store/reducers/order.js
@@ -0,0 +1,25 @@
+import * as actionTypes from "../actions/actionTypes";
+
+const intitalState = {
+  orders: [],
+  loading: false,
+};
+
+export const orderReducer = (state = intitalState, action) => {
+  switch (action.type) {
+    case actionTypes.PURCHASE_BURGER_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        orders: state.orders.concat({
+          ...action.orderData,
+          id: action.orderId,
+        }),
+      };
+    case actionTypes.PURCHASE_BURGER_FAILED:
+      break;
+    default:
+      break;
+  }
+  return state;
+};
