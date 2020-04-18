37615e7010627ac9bd03932dac319b93891dc1dc
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2020-04-18 12:24:18 +0530

    Added actions for handling Fetching ingredients from DB.
    - Added action types SET_INGREDIENT and FETCH_INGREDIENTS_FAILED.
    - Added respective action creators and middleware for fetching
    ingredients.
    - Made reducer's initial state : null and added state : error to catch fetching errors.

--- src/store/actions/burgerBuilder.js
+++ src/store/actions/burgerBuilder.js
@@ -1,4 +1,5 @@
 import * as actionTypes from "./actionTypes";
+import axios from "../../axios-orders";
 
 export const addIngredient = (name) => ({
   type: actionTypes.ADD_INGREDIENT,
@@ -8,3 +9,23 @@ export const removeIngredient = (name) => ({
   type: actionTypes.REMOVE_INGREDIENT,
   ingredientName: name,
 });
+
+export const setIngredients = (ingredients) => ({
+  type: actionTypes.SET_INGREDIENTS,
+  ingredients: ingredients,
+});
+
+export const fetchIngredientsFailed = () => ({
+  type: actionTypes.FETCH_INGREDIENTS_FAILED,
+});
+
+export const initIngredients = () => (dispatch) => {
+  axios
+    .get("/ingredients.json")
+    .then((response) => {
+      dispatch(setIngredients(response.data));
+    })
+    .catch((error) => {
+      dispatch(fetchIngredientsFailed());
+    });
+};
