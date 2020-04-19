e0956f9cc5f5fadc7c478ad66323c17431f77e12
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2020-04-19 12:40:38 +0530

    Added Auth Form for User Authentication.
    - Added Auth Component for handling User Authentication.
    - Added state: controls for controls of the Auth form.
    - Added state: formisValid for checking overall form validity.
    - Added route and navigation for Auth Form.

---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,127 @@
+import React, { Component } from "react";
+
+import Input from "../../components/UI/Input/Input";
+import Button from "../../components/UI/Button/Button";
+
+import styles from "./Auth.module.css";
+
+export class Auth extends Component {
+  state = {
+    controls: {
+      email: {
+        elementType: "input",
+        elementConfig: {
+          type: "email",
+          placeholder: "Your Email",
+        },
+        value: "",
+        validation: {
+          required: true,
+        },
+        valid: false,
+        interacted: false,
+      },
+      password: {
+        elementType: "input",
+        elementConfig: {
+          type: "password",
+          placeholder: "Password",
+          autoComplete: "off",
+        },
+        value: "",
+        validation: {
+          required: true,
+          minLength: 6,
+        },
+        valid: false,
+        interacted: false,
+      },
+    },
+    formIsValid: false,
+  };
+
+  inputChangedHandler = (event, inputId) => {
+    console.log(event.target.value);
+    const updatedForm = { ...this.state.controls };
+    const updatedFormElement = { ...updatedForm[inputId] };
+    updatedFormElement.interacted = true;
+    updatedFormElement.value = event.target.value;
+    updatedFormElement.valid = this.checkValidity(
+      updatedFormElement.value,
+      updatedFormElement.validation
+    );
+    updatedForm[inputId] = updatedFormElement;
+    this.setState({
+      controls: updatedForm,
+    });
+    const validity = this.checkFormIsValid(updatedForm);
+    if (validity !== this.state.formIsValid) {
+      this.setState({
+        formIsValid: validity,
+      });
+    }
+  };
+
+  checkFormIsValid = (updatedOrderForm) => {
+    let formIsValid = true;
+    for (let inputIdentifier in updatedOrderForm) {
+      formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+    }
+    return formIsValid;
+  };
+
+  checkValidity(value, rules) {
+    let isValid = true;
+    if (rules) {
+      if (rules.required) {
+        isValid = value.trim() !== "" && isValid;
+      }
+      if (rules.minLength) {
+        isValid = value.length >= rules.minLength && isValid;
+      }
+      if (rules.maxLength) {
+        isValid = value.length <= rules.minLength && isValid;
+      }
+    }
+    return isValid;
+  }
+
+  render() {
+    const formElementArray = [];
+    for (let key in this.state.controls) {
+      formElementArray.push({
+        id: key,
+        config: this.state.controls[key],
+      });
+    }
+    const form = (
+      <form>
+        {formElementArray.map((element) => (
+          <Input
+            key={element.id}
+            name={element.id}
+            elementType={element.config.elementType}
+            elementConfig={element.config.elementConfig}
+            value={element.config.elementValue}
+            changed={(event) => this.inputChangedHandler(event, element.id)}
+            invalid={!element.config.valid}
+            shouldValidate={element.config.validation}
+            interacted={element.config.interacted}
+          />
+        ))}
+        <Button btnType="Success" disabled={!this.state.formIsValid}>
+          LogIn
+        </Button>
+      </form>
+    );
+    return (
+      <div className={styles.Auth}>
+        <h1>Authentication</h1>
+        <br />
+        {form}
+      </div>
+    );
+  }
+}
+
+export default Auth;
