309e2ba42e08cffdada262c59b45582d9e5efb26
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2020-04-19 22:09:14 +0530

    Readded Strict mode

--- src/index.js
+++ src/index.js
@@ -28,14 +28,13 @@ const store = createStore(
 );
 
 ReactDOM.render(
-  //<React.StrictMode>
+  <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-  //</React.StrictMode>
-  ,
+  </React.StrictMode>,
   document.getElementById("root")
 );
 
