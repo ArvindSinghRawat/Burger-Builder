820e16ca6e662bbe600d8a141a1ea8e113aa38d2
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2020-04-22 11:52:55 +0530

    Added Saga Middleware : logoutSaga in Index.
    - Renamed and Exported Saga Generator: from logout to logoutSaga.
    - Imported and used in index.js.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
 
 import "./index.css";
 
@@ -13,6 +14,8 @@ import * as serviceWorker from "./serviceWorker";
 import burgerBuilderReducer from "./store/reducers/burgerBuilder";
 import orderReducer from "./store/reducers/order";
 import authReducer from "./store/reducers/auth";
+
+import { logoutSaga } from "./store/sagas/auth";
 
 const composeEnhancers =
   (process.env.NODE_ENV === "development"
@@ -25,10 +28,14 @@ const rootReducer = combineReducers({
   auth: authReducer,
 });
 
+const sagaMiddleware = createSagaMiddleware();
+
 const store = createStore(
   rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
+  composeEnhancers(applyMiddleware(thunk, sagaMiddleware))
 );
+
+sagaMiddleware.run(logoutSaga);
 
 ReactDOM.render(
   <React.StrictMode>
