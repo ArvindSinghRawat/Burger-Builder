aa0b2f8c3689d3722ebe48d4d9c727c3d8cf2c6c
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2020-04-24 00:17:11 +0530

    Converted class based Burger Ingredient to Functional Component.
    - Refactored Order Summary.

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -3,32 +3,32 @@ import Aux from "../../../hoc/Auxiliary/Auxiliary";
 import Button from "../../UI/Button/Button";
 
 const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients).map(
-    (ingredientKey) => (
-      <li key={ingredientKey}>
-        <span style={{ textTransform: "capitalize" }}>{ingredientKey}</span> :{" "}
-        <b>{props.ingredients[ingredientKey]}</b>
-      </li>
-    )
-  );
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients : </p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        <strong>Total Price : $ {props.price.toFixed(2)}</strong>
-      </p>
-      <br />
-      <p>Continue to Checkout</p>
-      <Button btnType="Success" clicked={props.purchseContinued}>
-        CONTINUE
-      </Button>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>
-        CANCEL
-      </Button>
-    </Aux>
-  );
+   const ingredientSummary = Object.keys(props.ingredients).map(
+      (ingredientKey) => (
+         <li key={ingredientKey}>
+            <span style={{ textTransform: "capitalize" }}>{ingredientKey}</span>{" "}
+            : <b>{props.ingredients[ingredientKey]}</b>
+         </li>
+      )
+   );
+   return (
+      <Aux>
+         <h3>Your Order</h3>
+         <p>A delicious burger with the following ingredients : </p>
+         <ul>{ingredientSummary}</ul>
+         <p>
+            <strong>Total Price : $ {props.price.toFixed(2)}</strong>
+         </p>
+         <br />
+         <p>Continue to Checkout</p>
+         <Button btnType="Success" clicked={props.purchseContinued}>
+            CONTINUE
+         </Button>
+         <Button btnType="Danger" clicked={props.purchaseCancelled}>
+            CANCEL
+         </Button>
+      </Aux>
+   );
 };
 
 export default orderSummary;
