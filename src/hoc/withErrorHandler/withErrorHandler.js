c052a6b2cc37f4a4d4fe441b83f97c814288f549
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2020-04-24 00:29:58 +0530

    Moved httpErrorHandler Logic to its own file.
    - Created custom hook: useHttpErrorHandler for catching request-response errors.
    - Used it in withErrorHandler.

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,42 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 import Modal from "../../components/UI/Modal/Modal";
 import Aux from "../Auxiliary/Auxiliary";
+import useHttpErrorHandler from "../../hooks/httpErrorHandler";
 
 const withErrorHandler = (WrappedComponent, axios) => {
    return (props) => {
-      const [error, setError] = useState(null);
-
-      const requestInterceptor = axios.interceptors.request.use((request) => {
-         setError(null);
-         return request;
-      });
-
-      const responseInterceptor = axios.interceptors.response.use(
-         (response) => response,
-         (err) => {
-            setError(err);
-            return Promise.reject(err);
-         }
-      );
-
-      useEffect(() => {
-         return () => {
-            axios.interceptors.request.eject(requestInterceptor);
-            axios.interceptors.response.eject(responseInterceptor);
-         };
-      }, [requestInterceptor, responseInterceptor]);
-
-      const errorConfirmedHandler = () => {
-         setError(null);
-      };
+      const [error, clearError] = useHttpErrorHandler(axios);
 
       return (
          <Aux>
-            <Modal
-               show={error ? true : false}
-               modalClosed={errorConfirmedHandler}
-            >
+            <Modal show={error ? true : false} modalClosed={clearError}>
                {error ? error.message : null}
             </Modal>
             <WrappedComponent {...props} />
